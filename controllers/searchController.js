ec2aecc2fff1a10ec387e630c1fe7634b14cd0bb
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-11-22 12:37:06 +0530

    change query parameter

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -3,7 +3,7 @@ import Car from "../models/Car.js";
 export const getSearchResults = async (req, res, next) => {
     try {
         const searchResults = await Car.find(
-            {carName: { $regex: req.query.query, $options: 'i'}}
+            {carNo: { $regex: req.query.query, $options: 'i'}}
         ).limit(20)
 
         res.status(200).json(searchResults)
